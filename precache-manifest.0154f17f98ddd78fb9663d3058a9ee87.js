557a58ad87d383698ab19a038c2f30040f92576b
chandu <chandugnanadeep@gmail.com>
2020-01-19 02:07:46 -0700

    Updates

---
+++ precache-manifest.0154f17f98ddd78fb9663d3058a9ee87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20b0348d2eb8c21e5d6be8cc4c94a68",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "be1415c70c3ce423e16d",
+    "url": "/home/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/home/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/home/static/js/2.f175dcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be1415c70c3ce423e16d",
+    "url": "/home/static/js/main.826cd147.chunk.js"
+  },
+  {
+    "revision": "380e9afaf55618a537c5",
+    "url": "/home/static/js/runtime-main.bf5ca5dd.js"
+  }
+]);
